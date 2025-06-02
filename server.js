347b4ebf53c88fbf3820ce01303e08d1b3283ab5
A Cottrill <a.rcottrill521@gmail.com>
2025-06-02 15:58:43 -0400

    chore: add dotenv to boilerplate

--- server.js
+++ server.js
@@ -8,6 +8,7 @@
 var fs = require('fs');
 var express = require('express');
 var app = express();
+require('dotenv').config();
 
 if (!process.env.DISABLE_XORIGIN) {
   app.use(function(req, res, next) {
